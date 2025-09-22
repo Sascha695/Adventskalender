7301ca60d1c870e6924a1a1d571babe8610c0135
Sascha695 <sascha.kaiser12@gmail.com>
2025-09-22 12:26:40 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -2,6 +2,6 @@
 window.ADVENT_CONFIG = {
   PASSWORD: "Schneeflocke",
   FINAL_PHRASE: "FROHE WEIHNACHTEN LIEBLING",
-  BASE_URL: "https://example.com/advent",
+  BASE_URL: "https://Sascha695.github.io/adventskalender",
   TITLE: "Adventskalender – Für dich ❤️"
 };
