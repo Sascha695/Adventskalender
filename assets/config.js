8f6b0ee2d7f6770d13c8c68b85af62909ae0fc53
Sascha695 <sascha.kaiser12@gmail.com>
2025-11-19 00:10:04 +0100

    Delete assets directory

--- assets/config.js
+++
@@ -1,7 +0,0 @@
-
-window.ADVENT_CONFIG = {
-  PASSWORD: "Schneeflocke",
-  FINAL_PHRASE: "FROHE WEIHNACHTEN LIEBLING",
-  BASE_URL: "https://Sascha695.github.io/adventskalender",
-  TITLE: "Adventskalender – Für dich ❤️"
-};
